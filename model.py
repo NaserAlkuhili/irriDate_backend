3f9c29cbd97ea949cc32bab6ce94e2c11e1bda5e
Naser Alkuhili <k4gam3r@gmail.com>
2024-09-14 21:09:14 -0500

    first commit

---
+++ model.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+# Example training data (X and y)
+X = np.array([[0.2], [0.8], [0.5], [0.3], [0.7], [0.1]])
+y = np.array([0, 1, 0, 0, 1, 0])
+
+# Create and train the model
+model = LogisticRegression()
+model.fit(X, y)
+
+# Function to predict 0 or 1
+def predict(input_value):
+    prediction = model.predict(np.array([[input_value]]))
+    return prediction[0]
+
